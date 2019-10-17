6a325e415a8cc8391b981743e7e5e2ec0e65577c
FabianSoto <sotofabian932@gmail.com>
2019-10-17 12:48:46 -0500

    MiPrimerCodigoEnJava

--- Ejemplo2/src/ejemplo2/Ejemplo2.java
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -16,6 +16,19 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String primer_nombre;
+        primer_nombre = "Fabian Antonio";
+        String apellidos;
+        apellidos = "Soto Jimenez";
+        int edad;
+        edad = 17;
+        String universidad;
+        universidad = "Universidad Tecnica Particular de Loja";
+        System.out.println(primer_nombre);
+        System.out.println(apellidos);
+        System.out.println(edad);
+        System.out.println(universidad);
+
     }
-    
+
 }
